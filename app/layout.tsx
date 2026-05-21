c8a4d48e51044a5c03f4681f5e562d8ec9bf1d6d
YOAKE <129704443+SaeBye0k@users.noreply.github.com>
2026-05-21 14:19:32 +0900

    Change-page-title-name

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Chanho Archive",
+  title: "Saebyeok Archive",
   description: "프로젝트와 개발 기록을 쌓아두는 게시글형 포트폴리오",
 };
 
